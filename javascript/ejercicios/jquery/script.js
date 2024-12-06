d0bcd8fd3e34c8f7739280b3db456a557f6261ca
tobimonges <tobiasmonges@pm.me>
2024-12-06 17:57:07 -0300

    rick and morty modificado v2

--- javascript/ejercicios/jquery/script.js
+++ javascript/ejercicios/jquery/script.js
@@ -1,5 +1,5 @@
 
-
+/*
 const btn = document.getElementById('btnFetchCharacters');
 
 const div = document.getElementById('characters');
@@ -15,3 +15,42 @@ function renderCharacters(characters) {
         div.innerHTML += `<img src="${ch.image}"> `;
     });
 }
+*/
+/*
+const resourceUrl = 'https://rickandmortyapi.com/api/character/';
+
+$(document).ready(function() {
+    $("button").click(function() {
+        $(".character-list").toggleClass("show");
+    });
+});
+$.ajax({
+    url: resourceUrl,
+    method: 'GET',
+    success: function(response) {
+      const html = response.results.map(item =>
+        `<li>
+          <img src="${item.image}" alt="" />
+          ${item.name}
+        </li>`
+      );
+      
+      $('.character-list').html(html.join(''));
+    },
+    error: function(err) {
+      console.log(err);
+    }
+  });
+
+  */
+
+const btn = document.getElementById('boton');
+const div = document.getElementById('personajes')
+
+btn.addEventListener('click', () => {
+    const xhr = new XMLHttpRequest();
+
+
+
+
+})
